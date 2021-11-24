7b1f0ed37cd9d4be0ca931780edab5e31462ae2f
Pedro Lopes <lopescostapedrohenrique@gmail.com>
2021-11-24 20:48:05 -0300

    primeiro fim

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,4 +1,5 @@
-const defaultUser = 'httpspedroh';
+const defaultUser = 'httpspedroh',
+      authorizationKey = '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';
 
 loadUserData(defaultUser);
 
@@ -19,30 +20,35 @@ function loadUserData(user)
 
     xhrUser.onload = showUserData;
     xhrUser.open('GET', `https://api.github.com/users/${user}`);
-    xhrUser.setRequestHeader('Authorization', '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');
+    xhrUser.setRequestHeader('Authorization', `token ${authorizationKey}`);
     xhrUser.send();
 
     xhrRep.onload = showUserRepos;
     xhrRep.open('GET', `https://api.github.com/users/${user}/repos`);
-    xhrRep.setRequestHeader('Authorization', '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');
+    xhrRep.setRequestHeader('Authorization', `token ${authorizationKey}`);
     xhrRep.send();
 }
 
 // ------------------------------------------------------------------------------------------------------------------ //
 
-function showUserData () {
+function showUserData() {
 
     if(xhrUser.status == 404) return alert(`O usuário "${searchInput.value}" não foi encontrado!`);
+
+    console.log(xhrUser.status);
 
     let text = '';
     let elem_change;
     let data = JSON.parse(this.responseText);
     let plural = 's';
 
+    console.log(data);
+
     // -------- //
 
     elem_change = document.getElementById('profile_img');
     elem_change.src = data.avatar_url;
+    elem_change.alt = data.login;
 
     // -------- //
 
@@ -75,24 +81,31 @@ function showUserData () {
 
     elem_change = document.getElementById('profile_org');
 
-    if(data.company == null) elem_change.remove();
+    if(data.company == null) elem_change.style.visibility = "hidden";
     else
     {
         if(data.company[0] == '@') elem_change.innerHTML = `<i class="far fa-building"></i><a href="https://github.com/${data.company.substring(1)}" target="_blank"><b>${data.company}</b></a>`;
         else elem_change.innerHTML = `<i class="far fa-building"></i>${data.company}`;
+
+        elem_change.style.visibility = "visible";
     }
 
     // -------- //
 
     elem_change = document.getElementById('profile_location');
 
-    if(data.location == null) elem_change.remove();
-    else elem_change.innerHTML = `<i class="fas fa-map-marker-alt"></i><a href="https://www.google.com/maps/place/${data.location}" target="_bl">${data.location}</a>`;
+    if(data.location == null) elem_change.style.visibility = "hidden";
+    else 
+    {
+        elem_change.innerHTML = `<i class="fas fa-map-marker-alt"></i><a href="https://www.google.com/maps/place/${data.location}" target="_bl">${data.location}</a>`;
+
+        elem_change.style.visibility = "visible";
+    }
 }
 
 // ------------------------------------------------------------------------------------------------------------------ //
 
-function showUserRepos () {
+function showUserRepos() {
 
     if(xhrRep.status == 404) return false;
 
@@ -110,29 +123,51 @@ function showUserRepos () {
         let dateCreated = new Date(rep.created_at);
         let dateUpdated = new Date(rep.updated_at);
 
-        if(rep.description == null) desc = "<i>No description provided.</i>";
-        else desc = rep.description;
+        // ---------------------- //
 
-        text += `<span class="rep_card col-12 col-lg-6 d-flex justify-content-center">
-            <div class="card bg-light mb-3">
-                <div class="card-header rep_title">
-                    <i class="rep_iconRep far fa-folder"></i><b>${rep.name}</b>`;
+        let totalCommits;
+        let xhrCommits = new XMLHttpRequest();
 
-        if(rep.language != null) text += `<span class="rep_lang">${rep.language}</span>`;
+        xhrCommits.onload = function () {
 
-        text += `</div>
-                <div class="card-body">
-                    <p class="card-text">${desc}</p>
-                    <p class="card-text rep_createdOn"><small class="text-muted">Created on: ${dateCreated.toLocaleString()}
-                    </br>Updated on: ${dateUpdated.toLocaleString()}</small></p>
-                    <span class="rep_link"><button class="btn"><a href="https://github.com/${rep.owner.login}/${rep.name}" target="_blank"><i class="fab fa-github"></i>View repository</a></button></span>`
-        
-        if(rep.has_pages == true) text += ` <span class="rep_link"><button class="btn"><a href="https://${rep.owner.login}.github.io/${rep.name}/" target="_blank"><i class="fas fa-tv"></i>View website</a></button></span>`;
+            let dataCommits = JSON.parse(this.responseText);
+            totalCommits = dataCommits.length;
 
-        text += `</div></div></span>`;
+            // ---------------------- //
+
+            if(rep.description == null) desc = "<i>No description provided.</i>";
+            else desc = rep.description;
+
+            text += `<span class="rep_card col-12 col-lg-6 d-flex justify-content-center">
+                <div class="card bg-light mb-3">
+                    <div class="card-header rep_title">
+                        <i class="rep_iconRep far fa-folder"></i><b>${rep.name}</b>`;
+
+            if(rep.language != null) text += `<span class="rep_lang">${rep.language}</span>`;
+            if(totalCommits != 0) 
+            {
+                plural = totalCommits == 1 ? '' : 's';
+                text += ` <span class="rep_commits">${totalCommits} commit${plural}</span>`;
+            }
+
+            text += `</div>
+                    <div class="card-body">
+                        <p class="card-text">${desc}</p>
+                        <p class="card-text rep_createdOn"><small class="text-muted">Created on: ${dateCreated.toLocaleString()}
+                        </br>Updated on: ${dateUpdated.toLocaleString()}</small></p>
+                        <span class="rep_link"><button class="btn"><a href="https://github.com/${rep.owner.login}/${rep.name}" target="_blank"><i class="fab fa-github"></i>View repository</a></button></span>`
+            
+            if(rep.has_pages == true) text += ` <span class="rep_link"><button class="btn"><a href="https://${rep.owner.login}.github.io/${rep.name}/" target="_blank"><i class="fas fa-tv"></i>View website</a></button></span>`;
+
+            text += `</div></div></span>`;
+
+            elem_change.innerHTML = text;
+        }
+
+        xhrCommits.open('GET', `https://api.github.com/repos/${rep.owner.login}/${rep.name}/commits`);
+        xhrCommits.setRequestHeader('Authorization', `token ${authorizationKey}`);
+        xhrCommits.send();
     }
-
-    elem_change.innerHTML = text;
 }
 
 // ------------------------------------------------------------------------------------------------------------------ //
